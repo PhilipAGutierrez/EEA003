6a9dc17eb9ae5aa41f41a216f15dbd9d222d8204
Philip Gutierrez <>
2022-04-22 16:00:50 -0500

    includes work on assignment 1 part 1

--- assignment1/code/question1.m
+++ assignment1/code/question1.m
@@ -1,5 +1,32 @@
 %-----------------------------------------------------
-%% Question 1 - Find the transfer function
+%% 2-Link RR Robot Arm
 %-----------------------------------------------------
 
-b = 3 + 4;
+% Specify rotation angles
+theta1 = 0;
+theta2 = pi ./ 2;
+
+% Links lengths are not provided, so set to length 1.
+a1 = 1;
+a2 = 1;
+
+% Rotation matrix for Frame 1
+r10 = rotz(theta1)*rotx(90)*roty(0)*rotz(0);
+
+% Rotation matrix for Frame 2
+r21 = rotz(theta2)*rotx(0)*roty(0)*rotz(0);
+
+% Translation vector for Frame 1
+p10 = [0 0 a1]';
+
+% Translation vector for Frame 2
+p21 = [a2*cos(theta2) a2*sin(theta2) 0]';
+
+% Create the transformation matrix H10
+H10 = [r10 p10; [0 0 0] [1]];
+
+% Create the transformation matrix H21
+H21 = [r21 p21; [0 0 0] [1]];
+
+% Calculate the system transformation matrix H20
+H20 = H10 * H21;
--- assignment1/code/question2.m
+++ assignment1/code/question2.m
@@ -1,5 +1,14 @@
 %-----------------------------------------------------
-%% Question 2 - Find the transfer function
+%% Test using a sample point
 %-----------------------------------------------------
 
-a = 1 + 2;
+% Choose a random point in Frame 2
+p2x = 0;
+p2y = 0;
+p2z = 0;
+p2 = [p2x p2y p2z 1]';
+
+% Translate thispoint into Frame 0
+p0 = H20 * p2
+
+% It works!
